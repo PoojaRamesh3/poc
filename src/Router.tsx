fdb695d6b99cf13eaf986bd27160ee2f932ba626
Pooja R <pooja.ramesh@imaginelearning.com>
2023-04-14 17:34:24 +0530

    Signin Page in Process

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./screens/LoginPage";
+import HomePage from "./screens/HomePage";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LoginPage />} />
+        <Route path="/home" element={<HomePage />} />
       </Routes>
     </BrowserRouter>
   );
