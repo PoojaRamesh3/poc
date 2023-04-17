f9cbafef4c35639aa6acbc5a6426a8a36e3c843b
Pooja R <pooja.ramesh@imaginelearning.com>
2023-04-17 09:53:36 +0530

    Signin Page in Process

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,10 +1,37 @@
-import FormInput from "./FormInput";
-import SignInButton from "./SignInButton";
 import SocialMediaButton from "./SocialMediaButton";
 import Link from "./Link";
 import CopyRight from "./CopyRight";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Form = () => {
+  const navigate = useNavigate();
+
+  const [loginInfo, setLoginInfo] = useState([]);
+  const [userData, setData] = useState("");
+
+  useEffect(() => {
+    fetch("https://64393d6f4660f26eb1adef4f.mockapi.io/users")
+      .then((res) => res.json())
+      .then((result) => {
+        setLoginInfo(result);
+      });
+  }, []);
+
+  const data = loginInfo.filter((res) => {
+    return JSON.stringify(res)
+      .toLocaleLowerCase()
+      .match(userData.toLocaleLowerCase());
+  });
+
+  if (data != null && userData === "pooja.ramesh331@gmail.com") {
+    navigate("/home");
+  }
+
+  const submitHandler = (event: any) => {
+    event.preventDefault();
+  };
+
   return (
     <div className="p-12 bg-white mx-auto rounded-3xl w-96 ">
       <div className="mb-7">
@@ -13,15 +40,30 @@ const Form = () => {
           Don't have an account? <Link text={"Sign Up"} />
         </p>
       </div>
-      <form className="space-y-6">
-        <FormInput placeholder={"Email"} />
-        <FormInput placeholder={"Password"} />
+      <form className="space-y-6" onSubmit={submitHandler}>
+        <input
+          className="w-full text-sm  px-4 py-3 bg-gray-200 focus:bg-gray-100 border  border-gray-200 rounded-lg focus:outline-none focus:border-purple-400"
+          type=""
+          placeholder="Email"
+          onChange={(e) => setData(e.target.value)}
+        />
+        <input
+          className="w-full text-sm  px-4 py-3 bg-gray-200 focus:bg-gray-100 border  border-gray-200 rounded-lg focus:outline-none focus:border-purple-400"
+          type=""
+          placeholder="Password"
+          onChange={(e) => setData(e.target.value)}
+        />
         <div className="flex items-center justify-between">
           <div className="text-sm ml-auto">
             <Link text={"Forgot your password?"} />
           </div>
         </div>
-        <SignInButton label={"Sign in"} />
+        <button
+          type="submit"
+          className="w-full flex justify-center bg-purple-800  hover:bg-black text-gray-100 p-3  rounded-lg tracking-wide font-semibold  cursor-pointer transition ease-in duration-500"
+        >
+          Sign in
+        </button>
         <div className="flex items-center justify-center space-x-2 my-5">
           <span className="h-px w-16 bg-gray-200"></span>
           <span className="text-gray-500 font-normal">or</span>
