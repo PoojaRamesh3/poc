21cae1aaa8b01781125cf84b293037019c6e543b
Pooja R <pooja.ramesh@imaginelearning.com>
2023-04-18 10:52:58 +0530

    Singin Page working functionality

--- src/screens/HomePage.tsx
+++ src/screens/HomePage.tsx
@@ -4,7 +4,11 @@ const HomePage = () => {
   return (
     <>
       <NavBar />
-      <h1>Welcome to Home Page!</h1>
+      <div className="flex items-center justify-center h-screen">
+        <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
+          <h1>Welcome to Home Page!</h1>
+        </div>
+      </div>
     </>
   );
 };
