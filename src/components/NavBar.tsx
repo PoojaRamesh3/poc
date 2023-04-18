634e0de91ed3beb0f12d63dccaefca0dc4026dab
Pooja R <pooja.ramesh@imaginelearning.com>
2023-04-18 14:54:00 +0530

    Initial Commit

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,6 +2,8 @@ import { Link } from "react-router-dom";
 
 const NavBar = () => {
   const pooja = require("../assets/pooja.jfif");
+  const logo = require("../assets/Logo.svg.png");
+  const bell = require("../assets/bell.png");
 
   const sideBarLists = [
     {
@@ -15,35 +17,65 @@ const NavBar = () => {
       url: "/teams",
     },
     {
-      id: 4,
+      id: 3,
       title: "Projects",
       url: "/projects",
     },
     {
-      id: 5,
+      id: 4,
       title: "Calendar",
       url: "/calendar",
     },
   ];
 
   return (
-    <div className="flex flex-row">
-      <ul className="">
+    <div className="flex flex-row justify-between items-center bg-gray-800 text-white font-bold">
+      <ul className="flex flex-row justify-between items-center">
         <li className="nav-item">
-          {sideBarLists.map((item, index) => (
+          <img
+            src={logo}
+            alt="Logo"
+            className="nav-link p-4"
+            width={75}
+            height={75}
+          />
+        </li>
+        {sideBarLists.map((item, index) => (
+          <li className="nav-item">
             <Link
               key={index}
               to={item.url}
-              className="nav-link"
+              className="nav-link p-4 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
               aria-current="page"
             >
               {item.title}
             </Link>
-          ))}
-        </li>
+          </li>
+        ))}
       </ul>
-      <ul>
-        <li>{pooja}</li>
+      <ul className="flex flex-row justify-between items-center">
+        <li className="nav-item p-4">
+          <button>
+            <img
+              src={bell}
+              alt="Notofication"
+              width={33}
+              height={33}
+              className="rounded-full bg-white"
+            />
+          </button>
+        </li>
+        <li className="nav-item p-4">
+          <button>
+            <img
+              src={pooja}
+              alt="Profile"
+              width={35}
+              height={35}
+              className="rounded-full border-2 border-white"
+            />
+          </button>
+        </li>
       </ul>
     </div>
   );
