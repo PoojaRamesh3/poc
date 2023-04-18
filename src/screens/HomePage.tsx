e151b14d4dcf3ae4f07b58a6b0a33cb8842229ed
Pooja R <pooja.ramesh@imaginelearning.com>
2023-04-18 12:10:57 +0530

    Singin Page working functionality

--- src/screens/HomePage.tsx
+++ src/screens/HomePage.tsx
@@ -5,7 +5,7 @@ const HomePage = () => {
     <>
       <NavBar />
       <div className="flex items-center justify-center h-screen">
-        <div className="bg-purple-900 bg-gradient-to-l from-gray-900 via-gray-900 to-purple-800 text-white font-bold rounded-lg border shadow-lg p-10">
+        <div className="bg-black text-white font-bold rounded-lg border shadow-lg p-10">
           <h1>Welcome to Home Page!</h1>
         </div>
       </div>
