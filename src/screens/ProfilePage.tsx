b9308c90f432079b9be0601ac1dcc20688fabf28
Pooja R <pooja.ramesh@imaginelearning.com>
2023-04-19 14:49:07 +0530

    Local Storage

--- src/screens/ProfilePage.tsx
+++ src/screens/ProfilePage.tsx
@@ -7,7 +7,7 @@ const ProfilePage = () => {
     <>
       <NavBar />
       <div className="flex items-center justify-center h-screen">
-        <img src={profile} />
+        <img src={profile} alt="" />
         <div className="bg-black text-white font-bold rounded-lg border shadow-lg p-10">
           <h1>Profile setting will be displayed here!</h1>
         </div>
--- src/screens/SettingPage.tsx
+++ src/screens/SettingPage.tsx
@@ -7,7 +7,7 @@ const SettingPage = () => {
     <>
       <NavBar />
       <div className="flex items-center justify-center h-screen">
-        <img src={setting} />
+        <img src={setting} alt="" />
         <div className="bg-black text-white font-bold rounded-lg border shadow-lg p-10">
           <h1>Settings options will be displayed here!</h1>
         </div>
