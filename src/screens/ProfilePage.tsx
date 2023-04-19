b9ba0f0ea428e86de6863d97f6d162912fb2eb7b
Pooja R <pooja.ramesh@imaginelearning.com>
2023-04-19 12:10:05 +0530

    Local Storage Updation

--- src/screens/ProfilePage.tsx
+++ src/screens/ProfilePage.tsx
@@ -7,7 +7,7 @@ const ProfilePage = () => {
     <>
       <NavBar />
       <div className="flex items-center justify-center h-screen">
-        <img src={profile} alt="" />
+        <img src={profile} />
         <div className="bg-black text-white font-bold rounded-lg border shadow-lg p-10">
           <h1>Profile setting will be displayed here!</h1>
         </div>
